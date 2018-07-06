b57100631d644d9396b7373f5816587832107016
L H Gilfoyle <l.gilfoyle@wlv.ac.uk>
2018-07-06 14:27:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  let ret = [];
+  for (var i = 0; i < musicians.length && i < instruments.length; i++) {
+    ret.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+}
